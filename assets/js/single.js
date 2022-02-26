d72b63a9a163689d6278fc8a7a036f0274fd826b
Chris <christopher.sarm15@gmail.com>
2022-02-26 12:58:48 -0500

    added displayWarning function

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -8,15 +9,16 @@ var getRepoIssues = function(repo) {
         if (response.ok) {
             response.json().then(function (data) {
                 displayIssues(data);
+
+                //check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         } else {
             alert("there was a problem with your reuqest!");
         }
-    });
-
-
-
-    
+    });    
 };
 
 var displayIssues = function(issues) {
@@ -54,6 +56,19 @@ var displayIssues = function(issues) {
         issueContainerEl.appendChild(issueEl);
     }
 
+};
+
+var displayWarning = function(repo) {
+    //add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent= "See more Issues on Github.com";
+    linkEl.setAttribute("href", "https://github.com/"+repo+"/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    //append to warning container
+    limitWarningEl.appendChild(linkEl);
 };
 
 getRepoIssues("facebook/react");
