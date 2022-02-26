7c4fb6eea4c85efb90bd77736010fe0bf58e2cfd
Chris <christopher.sarm15@gmail.com>
2022-02-26 14:05:29 -0500

    added error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,6 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -16,9 +17,22 @@ var getRepoIssues = function(repo) {
                 }
             });
         } else {
-            alert("there was a problem with your reuqest!");
+            document.location.replace("./index.html");
         }
     });    
+};
+
+var getRepoName = function() {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    console.log(repoName);
+
+    if(repoName) {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        document.location.replace("./index.html");
+    } 
 };
 
 var displayIssues = function(issues) {
@@ -71,4 +85,4 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
